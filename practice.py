ab04c59b22a28c11b9dfbaa147657e84562d7a01
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2025-09-13 16:52:36 +0000

    commit

--- practice.py
+++ practice.py
@@ -1,6 +1,35 @@
-num = input("Enter a two digit number ")
+# num = input("Enter a two digit number ")
 
-num_one = num[0]
-num_two = num[1]
+# num_one = num[0]
+# num_two = num[1]
 
-print(int(num_one) + int(num_two))
+# print(int(num_one) + int(num_two))
+
+
+# height = int(input("Enter your height "))
+# weight = float(input('Enter you weight '))
+# BMI = weight/(height**2)
+# print(BMI)
+
+# a,b = 4,7
+# c = (a+b) # 11
+# a += 2 # 6
+# print(a)
+# c += a # 11 + 6
+# c /= a
+# c // a
+# print(c)
+# # b %= a
+# print(b)
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# print("Array:", arr)
+# print(arr[0])
+# arr[1] = 90
+# print(arr)
+# # print("Memory address of first element:", arr.ctypes.data)
+
+arr = [10, 20, 30]
+print("ID of first element:", id(arr[0]))
+print("ID of second element:", id(arr[1]))
