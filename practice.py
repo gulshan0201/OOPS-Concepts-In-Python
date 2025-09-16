cb3f9a71eb4b1f049ad121763b12e23eb3a40158
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2025-09-16 14:27:37 +0000

    Operators

--- practice.py
+++ practice.py
@@ -40,13 +40,28 @@
 # for i in range(len(arr)):
 #     print(f"Index {i} has value {arr[i]}")
 
-# Identity operators in python
+# ---------------------------------------Identity operators in python------------------------------------------
 #identity operators in this below solution we will get a ouput True why? 
 #In this case as we can notice that both a and b have same value but the logic behind this is both the values 
 # of a and b are getting stored in the same values so in this case it will give true as python is a object oriented 
 # programing language the object is getting stored in the same id..  
-a = 5
-b = 5
-print(id(a)) #102962020996136
-print(id(b)) #102962020996136
-print(a  is b) #True
+# a = 5
+# b = 5
+# print(id(a)) #102962020996136
+# print(id(b)) #102962020996136
+# print(a  is b) #True
+
+
+#----------------------------Membership Operator------------------------
+# Operators of membership operators are "in" and "not in" 
+# Example 
+
+# name = "Gulshan" # In this string suppose we want to check weather the letter small "s" is present in the name or not
+# print("s" in name)  #True
+# print('b' in name) # False
+# print('c' not in name) #True
+
+# list_one = [1,90,2,6 ,89]
+# print(1 in list_one) #True
+# print(1 not in list_one) #False
+# print(3 not in list_one) #True
